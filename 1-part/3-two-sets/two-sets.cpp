368fdf9f29a320da6fcad2cea4a931eab30c6949
Julia <julia.palorinne@helsinki.fi>
2023-01-13 22:46:13 +0200

    task 3

--- 1-part/3-two-sets/two-sets.cpp
+++ 1-part/3-two-sets/two-sets.cpp
@@ -1,22 +1,44 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
 int main() {
   int n;
   cin >> n;
-  long a[n];
 
-  for (int i = 0; i < n; i++) {
-    cin >> a[i];
-  }
-
-  long moves = 0;
-  for (int i = 1; i < n; i++) {
-    if (a[i] < a[i-1]) {
-      moves += (a[i-1] - a[i]);
-      a[i] = a[i-1];
+  stringstream set1;
+  stringstream set2;
+  
+  bool possible = false;
+  int rem = n % 4;
+  long sum1 = 0;
+  long sum2 = 0;
+  int n1 = 0;
+  int n2 = 0;
+  if (rem == 0 || rem == 3) {
+    possible = true;
+    for (int i = n; i > 0; i--) {
+      if (sum1 <= sum2) {
+        sum1 += i;
+        set1 << i << " ";
+        n1++;
+      } else {
+        sum2 += i;
+        set2 << i << " ";
+        n2++;
+      }
     }
   }
-  cout << moves << endl;
+  
+  if (possible) {
+    cout << "YES" << endl;
+    cout << n1 << endl;
+    cout << set1.str() << endl;
+    cout << n2 << endl;
+    cout << set2.str() << endl;
+  } else {
+    cout << "NO" << endl;
+  }
 }
